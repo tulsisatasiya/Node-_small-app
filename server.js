c7f59ed79792a008421194bcf61770908854ef9e
Tulsi <tulsisatasiya085@gmail.com>
2023-11-06 13:49:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,73 @@
+// first
+
+// const http = require("http");
+// const port = 5000;
+
+// http.createServer((req, res) => {
+//     let data = "first server";
+//     console.log("server  started");
+
+//     res.write("hello world");
+//     res.write(data);
+//     res.end();
+//   }).listen(port);
+
+
+
+// seconde
+
+// const http = require("http");
+
+// http.createServer((req, res) => {
+//     let data = "seconde server";
+//     console.log("server started");
+
+//     res.write("hello :-");
+//     res.write(data);
+//     res.end();
+//   }).listen(5001);
+
+
+
+// third
+
+// const http = require("http");
+
+// http.createServer((req, res) => {
+//     let data = "third server";
+//     console.log("server started");
+
+//     res.write("hello :-");
+//     res.write(data);
+//     res.end();
+//   }).listen(5002);
+
+
+// fourth
+// const http = require("http");
+
+// http.createServer((req, res) => {
+//     let data = "fourth server";
+//     console.log("server started");
+
+//     res.write("hello :-");
+//     res.write(data);
+//     res.end();
+//   }).listen(5003);
+
+
+// fifth
+const http = require("http");
+
+http.createServer((req, res) => {
+    let data = "fifth server";
+    console.log("server started");
+
+    res.write("hello :-");
+    res.write(data);
+    res.end();
+  }).listen(5004);
+
+  
+
+ 
